d4551713971856fd462c4e9bc87fe14e17e8facb
Andrés Villa <robertoavilla@gmail.com>
2020-03-28 00:31:43 -0300

    Initialization

---
+++ CSharpUtilities.Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpUtilities.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int ToInt32(this string me)
+        {
+            try
+            {
+                return Int32.Parse(me);
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
