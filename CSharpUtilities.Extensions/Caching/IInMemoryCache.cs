a9b961817c584f312a2f5ba4575ea185b20453f6
Andrés Villa <robertoavilla@gmail.com>
2020-04-01 11:59:29 -0300

    Add Memory Cache Interface

---
+++ CSharpUtilities.Extensions/Caching/IInMemoryCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpUtilities.Extensions.Caching
+{
+    public interface IInMemoryCache
+    {
+        bool TryGetValue<T>(string entryKey, out T cacheEntry);
+
+        T Set<T>(string entryKey, T cacheEntry, InMemoryCacheOffset offset);
+
+        void Remove(string entryKey);
+    }
+}
---
+++ CSharpUtilities.Extensions/Caching/InMemoryCacheOffset.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpUtilities.Extensions.Caching
+{
+    public enum InMemoryCacheOffset
+    {
+        OneSecond,
+        TenSeconds,
+        ThirtySeconds,
+        OneMinute,
+        FiveMinutes,
+        FiftyMinutes,
+        ThirtyMinutes,
+        OneHour,
+        TwoHours,
+        SixHours,
+        TwelveHours,
+        OneDay,
+        NoLimit
+    }
+}
