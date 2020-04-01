3d0a79f6561754cb07128394e3b4a2950ee28540
Andrés Villa <robertoavilla@gmail.com>
2020-04-01 12:59:21 -0300

    Added Entity and Repository Classes

---
+++ CSharpUtilities.Entities.DataAccess.Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using CSharpUtilities.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CSharpUtilities.Entities.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        T GetById(Guid id);
+
+        IEnumerable<T> List();
+
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+
+        void Create(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
---
+++ Entities/CSharpUtilities.Entities.Interfaces/IAccesible.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpUtilities.Entities.Interfaces
+{
+    public interface IAccesible
+    {
+        long AccessCount { get; set; }
+    }
+}
