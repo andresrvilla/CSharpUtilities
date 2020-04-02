f9f207d3602d5ffa4144f333a3fb937454935d32
Andrés Villa <robertoavilla@gmail.com>
2020-04-02 00:34:01 -0300

    Add Abstract Repository Class

--- CSharpUtilities.Entities.DataAccess.Interfaces/IRepository.cs
+++ CSharpUtilities.Entities.DataAccess.Interfaces/IRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace CSharpUtilities.Entities.DataAccess.Interfaces
 {
@@ -9,14 +10,26 @@ namespace CSharpUtilities.Entities.DataAccess.Interfaces
     {
         T GetById(Guid id);
 
+        Task<T> GetByIdAsync(Guid id);
+
         IEnumerable<T> List();
+
+        Task<IEnumerable<T>> ListAsync();
 
         IEnumerable<T> List(Expression<Func<T, bool>> predicate);
 
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+
         void Create(T entity);
+
+        Task CreateAsync(T entity);
 
         void Delete(T entity);
 
+        Task DeleteAsync(T entity);
+
         void Update(T entity);
+
+        Task UpdateAsync(T entity);
     }
 }
---
+++ CSharpUtilities.Entities.DataAccess/Repository.cs
@@ -0,0 +1,55 @@
+﻿using CSharpUtilities.Entities.DataAccess.Interfaces;
+using CSharpUtilities.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace CSharpUtilities.Entities.DataAccess
+{
+    public abstract class Repository<T> : IRepository<T> where T : IEntity
+    {
+        public void Create(T entity)
+        {
+            Task.Run(() => CreateAsync(entity));
+        }
+
+        public void Delete(T entity)
+        {
+            Task.Run(() => DeleteAsync(entity));
+        }
+
+        public T GetById(Guid id)
+        {
+            return Task.Run(() => GetByIdAsync(id)).Result;
+        }
+
+        public IEnumerable<T> List()
+        {
+            return Task.Run(() => ListAsync()).Result;
+        }
+
+        public IEnumerable<T> List(Expression<Func<T, bool>> predicate)
+        {
+            return Task.Run(() => ListAsync(predicate)).Result;
+        }
+
+        public void Update(T entity)
+        {
+            Task.Run(() => UpdateAsync(entity));
+        }
+
+
+        public abstract Task CreateAsync(T entity);
+
+        public abstract Task DeleteAsync(T entity);
+
+        public abstract Task<T> GetByIdAsync(Guid id);
+
+        public abstract Task<IEnumerable<T>> ListAsync();
+
+        public abstract Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+
+        public abstract Task UpdateAsync(T entity);
+    }
+}
